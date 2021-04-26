c29e5737391e5742fe87625b78d42df27022d3e1
kajal1329 <72183627+kajal1329@users.noreply.github.com>
2021-04-26 14:49:51 +0530

    Add files via upload

---
+++ Signed-up.php
@@ -0,0 +1,16 @@
+<?php
+
+echo "You are Successfully Signed Up!!";
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+<p> <font style="color: red"> Please Sign in to Continue :) </p>
+<a class="btn btn-default" href="index.php"> Continue </a>
+</body>
+</html>
